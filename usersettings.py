42603b7a6a6c8ee794905ae68b467898ea4085cf
German Jimenez <germanjimenez84@gmail.com>
2014-11-27 16:08:00 -0300

    Create usersettings.py

---
+++ usersettings.py
@@ -0,0 +1,7 @@
+commands = {
+    'Sqliteman':'sqliteman',
+    'WxFormBuilder':'wxformbuilder',
+    'Sublime Text 2': 'sublime-text',
+    'Google Chrome': 'google-chrome',
+    'Eclipse': 'eclipse'
+    }
